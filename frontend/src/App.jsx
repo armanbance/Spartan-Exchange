95b528bbf013bcb84272ef801ade1736ee32ff3c
armanbance <armanbance@gmail.com>
2024-07-31 16:56:03 -0700

    configured tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+
+function App() {
+  return (
+    <>
+      <h1 className='text-orange-500 text-center'>Title Page</h1>
+    </>
+  )
+}
+
+export default App
