e581b64b8a9e2a78429518902e1aff9f34af4c7d
armanbance <armanbance@gmail.com>
2024-08-05 11:51:35 -0700

    created a logger and middleware files.

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const express = require('express')
 const app = express()
 require('express-async-errors')
 const cors = require('cors')
-const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
